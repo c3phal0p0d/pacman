32343d61ea3e84831b2ab20a36eef61a55e7dbdb
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-04-16 22:12:59 +1000

    Add 'Item ' class

---
+++ pacman/src/items/Item.java
@@ -0,0 +1,36 @@
+package src.items;
+
+import ch.aplu.jgamegrid.Location;
+
+import java.awt.*;
+
+public abstract class Item {
+
+    private Location location;
+    private String imageName;
+    private Color paintColor;
+
+    /**
+     * Instantiates a new 'Item'
+     * @param location the location of the item on the board
+     * @param imageName the filename of the item's sprite
+     * @param paintColor the color of the item
+     */
+    public Item(Location location, String imageName, Color paintColor) {
+        this.location = location;
+        this.imageName = imageName;
+        this.paintColor = paintColor;
+    }
+
+    // Getter Methods:
+    public Location getLocation() { return this.location; }
+    public String getImageName() { return this.imageName; }
+    public Color getPaintColor() { return this.paintColor; }
+
+    // Setter Methods:
+    public void setLocation(Location newLocation) { this.location = newLocation; }
+    public void setImageName(String newImageName) { this.imageName = newImageName; }
+    public void setPaintColor(Color newPaintColor) { this.paintColor = newPaintColor; }
+
+    //public abstract void causeEffect();
+}
